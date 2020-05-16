c81f9a1e1a24c46c197704df4d4b517227a8cc16
Wagner Junior <wagner.junior30@gmail.com>
2020-05-16 17:37:27 -0300

    :rocket: GraphQL UrlCreated subscription added

---
+++ app/GraphQL/Subscriptions/UrlCreated.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\GraphQL\Subscriptions;
+
+use App\Url;
+use Illuminate\Http\Request;
+use GraphQL\Type\Definition\ResolveInfo;
+use Nuwave\Lighthouse\Subscriptions\Subscriber;
+use Nuwave\Lighthouse\Schema\Types\GraphQLSubscription;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+class UrlCreated extends GraphQLSubscription
+{
+    /**
+     * Check if subscriber is allowed to listen to the subscription.
+     *
+     * @param  \Nuwave\Lighthouse\Subscriptions\Subscriber  $subscriber
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    public function authorize(Subscriber $subscriber, Request $request): bool
+    {
+        return true;
+    }
+
+    /**
+     * Filter which subscribers should receive the subscription.
+     *
+     * @param  \Nuwave\Lighthouse\Subscriptions\Subscriber  $subscriber
+     * @param  mixed  $root
+     * @return bool
+     */
+    public function filter(Subscriber $subscriber, $root): bool
+    {
+        return true;
+    }
+
+    /**
+     * Resolve the subscription.
+     *
+     * @param  \App\Url  $root
+     * @param  mixed[]  $args
+     * @param  \Nuwave\Lighthouse\Support\Contracts\GraphQLContext  $context
+     * @param  \GraphQL\Type\Definition\ResolveInfo  $resolveInfo
+     * @return mixed
+     */
+    public function resolve($root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo): Url
+    {
+        // Optionally manipulate the `$root` item before it gets broadcasted to
+        // subscribed client(s).
+
+        return $root;
+    }
+}
